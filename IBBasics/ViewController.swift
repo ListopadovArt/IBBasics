70bad3f5bc6f222b5059064113615780d0307e15
ART_List <artbdarm9@mail.ru>
2020-12-04 22:53:56 +0300

    added a new Label in IB

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -9,32 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
 
+    
     @IBOutlet weak var buttonRussiaO: UIButton!
     
     @IBOutlet weak var buttonReferenceO: UIButton!
     
-   
+    
+    @IBOutlet weak var labelCountry: UILabel!
+        
+    
+    // Adds label as a child view to `view`
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        buttonRussiaO.setTitleColor(.blue, for: .normal)
+        buttonRussiaO.setTitleColor(.green, for: .normal)
         buttonReferenceO.setTitleColor(.red, for: .normal)
         
+//        let label = UILabel(frame: CGRect(x: 50, y: 340, width: 200, height: 44))
+//           view.addSubview(label)
+// Программный способ добавления кнопки
     
         // Do any additional setup after loading the view.
     }
     
 
-
+    
     @IBAction func buttonRussia(_ sender: Any) {
-    
+        print("The button was pressed")
     }
-    
     @IBAction func referenceButton(_ sender: Any) {
-        
+        print("The button was pressed")
     }
-    
 }
 
