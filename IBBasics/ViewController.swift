6dc47503c22d5598883654557275961a55c4a488
ART_List <artbdarm9@mail.ru>
2020-12-04 12:36:16 +0300

    first commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by Artem Listopadov on 12/4/20.
+//  Copyright © 2020 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
